0605d0925795ccb3726805dfe33f61351b979256
Aleksandr Simakovych <aleksandr0721@outlook.com>
2020-07-02 16:13:06 -0500

    Connected API

--- App.js
+++ App.js
@@ -3,32 +3,63 @@ import { SafeAreaView, StatusBar, View, FlatList, Text, StyleSheet } from 'react
 
 import Search from './src/search';
 
+const token = "zCXHGpRehDU461S2pKnLMhYd";
+
 const App = () => {
   const [data, setData] = useState([]);
   const [text, setText] = useState("");
   const [list, setList] = useState([]);
 
   useEffect(() => {
+    _getContacts();
+  });
 
-  }, []);
+  useEffect(() => {
+    if (text == "") {
+      setData(list);
+      return;
+    }
+
+  }, [text]);
+
+  _getContacts = () => {
+    fetch('https://k-messages-api.herokuapp.com/api/v1/contacts', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setList(json.contacts);
+        setData(json.contacts);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   onChangeText = text => {
     setText(text);
-
   }
 
   renderItem = ({ item, index }) => {
+    const { name, email } = item;
     return (
-      <View style={styles.item}>
-        <Text numberOfLines={2}>Name</Text>
-        <View style={{ height: 10 }} />
-        <Text numberOfLines={2}>Email Address</Text>
+      <View style={styles.itemContainer}>
+        <View style={styles.item}>
+          <Text numberOfLines={2}>{name}</Text>
+          <View style={{ height: 10 }} />
+          <Text numberOfLines={2}>{email}</Text>
+        </View>
       </View>
     );
   }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView emulateUnlessSupported={false}>
       <StatusBar />
       <Search
         onChangeText={onChangeText}
@@ -37,6 +68,8 @@ const App = () => {
         {`Displaying ${data.length} of ${list.length} Contacts`}
       </Text>
       <FlatList
+        style={styles.alignItems}
+        keyExtractor={(_, index) => index.toString()}
         data={data}
         renderItem={renderItem}
       />
@@ -45,19 +78,20 @@ const App = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
   resultTxt: {
     width: '90%',
     alignContent: 'center',
     margin: 20
   },
+  itemContainer: {
+    width: '100%',
+    alignItems: 'center'
+  },
   item: {
     width: '90%',
     marginBottom: 10,
     borderWidth: 1,
-    padding: 10
+    padding: 10,
   }
 });
 
